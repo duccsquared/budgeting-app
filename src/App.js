8659b9434eddd03b132b64bb98bf9e37560a61f8
Mario Susanto <msus0004@student.monash.edu>
2022-11-22 02:32:42 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">
+          Budgeting Application
+        </h1>
+        <h2 className="text-xl font-bold">
+          Subheading
+        </h2>
+        <div style={{height: 20}}></div>
+        <p className="text-sm">insert text here</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
