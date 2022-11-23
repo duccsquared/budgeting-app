4af885575e9c262d2af52024fb1d68abf99a398a
Mario Susanto <msus0004@student.monash.edu>
2022-11-23 14:53:25 +0700

    created components for text

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import './App.css';
-import {BtnMain, BtnSecondary} from './components.js';
+import {BtnMain, BtnSecondary, H1, H2, H3, P} from './components.js';
 // function logBeep() {
 //   console.log(logBeep)
 // }
 
-function App() {
+function App() { // <div style={{height: 20}}></div>
     return (
         <div className="App">
             <header className="App-header">
-                <h1 className="text-3xl font-bold underline">
-                    Budgeting Application
-                </h1>
-                <h2 className="text-xl font-bold">
-                    Subheading
-                </h2>
-                <div style={{height: 20}}></div>
-                <p className="text-sm">insert text here</p>
+                <H1>Header 1</H1>
+                <H2>Header 2</H2>
+                <H3>Header 3</H3>
+                <P>paragraph</P>
                 <BtnMain onClick={() => console.log("beep")}>
                     bonk bonk
                 </BtnMain>
--- src/components.js
+++ src/components.js
@@ -4,21 +4,60 @@ import './index.css';
 export class BtnMain extends React.Component {
     render() {
         return (
-            <button onClick={this.props.onClick} className="bg-blue-600 hover:bg-blue-500 active:bg-blue-700 rounded-lg px-2 shadow-lg shadow-blue-800/50 hover:shadow-blue-800 active:shadow-inner">
+            <button onClick={this.props.onClick} className="text-sm bg-blue-600 hover:bg-blue-500 active:bg-blue-700 rounded-lg px-2 shadow-lg shadow-blue-800/50 hover:shadow-blue-800 active:shadow-inner">
                 {this.props.children}
             </button>
-            )
+        )
     }
 }
 
 export class BtnSecondary extends React.Component {
     render() {
         return (
-            <button onClick={this.props.onClick} className="bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg px-2 shadow-lg shadow-indigo-800/50 hover:shadow-indigo-800 active:shadow-inner">
+            <button onClick={this.props.onClick} className="text-sm bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg px-2 shadow-lg shadow-indigo-800/50 hover:shadow-indigo-800 active:shadow-inner">
                 {this.props.children}
             </button>
-            )
+        )
     }
 }
 
+export class H1 extends React.Component {
+    render() {
+        return (
+            <h1 className="text-6xl font-extrabold font-mono">
+                {this.props.children}
+            </h1>
+        )
+    }
+}
+
+export class H2 extends React.Component {
+    render() {
+        return (
+            <h2 className="text-4xl font-bold font-mono">
+                {this.props.children}
+            </h2>
+        )
+    }
+}
+
+export class H3 extends React.Component {
+    render() {
+        return (
+            <h3 className="text-2xl font-semibold font-mono">
+                {this.props.children}
+            </h3>
+        )
+    }
+}
+
+export class P extends React.Component {
+    render() {
+        return (
+            <p className="text-sm font-mono">
+                {this.props.children}
+            </p>
+        )
+    }
+}
 
