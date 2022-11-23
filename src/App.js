1a3a40fc248827f2908b0658d6a5f54442ba68f2
Mario Susanto <msus0004@student.monash.edu>
2022-11-23 14:04:26 +0700

    redesigned button slightly and created secondary button component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import './App.css';
-import Btn from './components.js';
-
+import {BtnMain, BtnSecondary} from './components.js';
 // function logBeep() {
 //   console.log(logBeep)
 // }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline">
-          Budgeting Application
-        </h1>
-        <h2 className="text-xl font-bold">
-          Subheading
-        </h2>
-        <div style={{height: 20}}></div>
-        <p className="text-sm">insert text here</p>
-        <Btn onClick={() => console.log("boop")}>
-          bonk bonk
-        </Btn>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 className="text-3xl font-bold underline">
+                    Budgeting Application
+                </h1>
+                <h2 className="text-xl font-bold">
+                    Subheading
+                </h2>
+                <div style={{height: 20}}></div>
+                <p className="text-sm">insert text here</p>
+                <BtnMain onClick={() => console.log("beep")}>
+                    bonk bonk
+                </BtnMain>
+                <BtnSecondary onClick={() => console.log("boop")}>
+                    beep boop
+                </BtnSecondary>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/components.js
+++ src/components.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './index.css';
 
-export default class Btn extends React.Component {
+export class BtnMain extends React.Component {
     render() {
         return (
-            <button onClick={this.props.onClick} className="bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg px-2 shadow-lg shadow-indigo-800/50 hover:shadow-indigo-800">
+            <button onClick={this.props.onClick} className="bg-blue-600 hover:bg-blue-500 active:bg-blue-700 rounded-lg px-2 shadow-lg shadow-blue-800/50 hover:shadow-blue-800 active:shadow-inner">
                 {this.props.children}
             </button>
             )
     }
 }
 
-
+export class BtnSecondary extends React.Component {
+    render() {
+        return (
+            <button onClick={this.props.onClick} className="bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg px-2 shadow-lg shadow-indigo-800/50 hover:shadow-indigo-800 active:shadow-inner">
+                {this.props.children}
+            </button>
+            )
+    }
+}
 
 
