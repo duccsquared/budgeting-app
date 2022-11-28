45a3f0bae1d55596497168dc9cfe031f3dad6282
Mario Susanto <msus0004@student.monash.edu>
2022-11-29 02:33:12 +0700

    tested out format for application

--- src/App.js
+++ src/App.js
@@ -12,55 +12,77 @@ function App() { // <div style={{height: 20}}></div>
                 <NavBar></NavBar>
             </header>
             <body className="App-body">
-                <H1>Header 1</H1>
-                <H2>Header 2</H2>
-                <H3>Header 3</H3>
-                <P>paragraph</P>
-                <BtnMain onClick={() => console.log("beep")}>
-                    bonk bonk
-                </BtnMain>
-                <BtnSecondary onClick={() => console.log("boop")}>
-                    beep boop
-                </BtnSecondary>
-                <Table>
-                    <TR1>
-                        <TH>Label</TH>
-                        <TH>Category</TH>
-                        <TH>Amount</TH>
-                    </TR1>
-                    <TR1>
-                        <TD>Lunch</TD>
-                        <TD>Food</TD>
-                        <TD>10</TD>
-                    </TR1>
-                    <TR2>
-                        <TD>Games</TD>
-                        <TD>Miscellaneous</TD>
-                        <TD>20</TD>
-                    </TR2>
-                    <TR1>
-                        <TD>Dinner</TD>
-                        <TD>Food</TD>
-                        <TD>12</TD>
-                    </TR1>
-                    <TR2>
-                        <TD>AC</TD>
-                        <TD>Electricity</TD>
-                        <TD>10</TD>
-                    </TR2>
-                </Table> 
-                <CheckBox>
-                    Cool CheckBox
-                </CheckBox>
-                <RadioBox name="beep" value="1">
-                    Radio 1
-                </RadioBox>
-                <RadioBox name="beep">
-                    Radio 2
-                </RadioBox>
-                <RadioBox name="beep">
-                    Radio 3
-                </RadioBox>
+                <div class="grid grid-cols-4 gap-4">
+                    <div class="col-span-4"></div>
+                    <div class="bg-zinc-700 col-span-2 row-span-3 flex shadow-md" justify-center>
+                        <div class="grow"></div>
+                        <div>
+                        <Table>
+                            <TR1>
+                                <TH>Label</TH>
+                                <TH>Category</TH>
+                                <TH>Amount</TH>
+                            </TR1>
+                            <TR1>
+                                <TD>Lunch</TD>
+                                <TD>Food</TD>
+                                <TD>10</TD>
+                            </TR1>
+                            <TR2>
+                                <TD>Games</TD>
+                                <TD>Miscellaneous</TD>
+                                <TD>20</TD>
+                            </TR2>
+                            <TR1>
+                                <TD>Dinner</TD>
+                                <TD>Food</TD>
+                                <TD>12</TD>
+                            </TR1>
+                            <TR2>
+                                <TD>AC</TD>
+                                <TD>Electricity</TD>
+                                <TD>10</TD>
+                            </TR2>
+                        </Table> 
+                        </div>
+                        <div class="grow"></div>
+                    </div>
+                    <div class="bg-zinc-700 shadow-md">
+                        <H1>Header 1</H1>
+                        <H2>Header 2</H2>
+                        <H3>Header 3</H3>
+                        <P>paragraph</P>
+
+
+                    </div>
+                    <div class="bg-zinc-700 shadow-md">
+                        <CheckBox>
+                            Cool CheckBox
+                        </CheckBox>
+                        <RadioBox name="beep" value="1">
+                            Radio 1
+                        </RadioBox>
+                        <RadioBox name="beep">
+                            Radio 2
+                        </RadioBox>
+                        <RadioBox name="beep">
+                            Radio 3
+                        </RadioBox>
+                    </div>
+                    <div class="bg-zinc-700 col-span-2 shadow-md">
+                        <BtnMain onClick={() => console.log("beep")}>
+                            bonk bonk
+                        </BtnMain>
+                    </div>
+                    <div class="bg-zinc-700 col-span-2 shadow-md">
+                        <BtnSecondary onClick={() => console.log("boop")}>
+                            beep boop
+                        </BtnSecondary>
+                    </div>
+                </div>
+
+
+
             </body>
         </div>
     );
