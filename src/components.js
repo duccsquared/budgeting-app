6dfde7816924ba3b754ecdbfda7de37032512f8e
Mario Susanto <msus0004@student.monash.edu>
2022-12-02 22:08:45 +0700

    fixed classes for components.js

--- src/components.js
+++ src/components.js
@@ -17,7 +17,7 @@ function genRandomID(length=16) {
 export class MainSection extends React.Component {
     render() {
         return (
-            <div className="bg-zinc-800 min-h-screen text-white text-center">
+            <div className={"bg-zinc-800 min-h-screen text-white text-center " + (this.props.className || "")}>
                 {this.props.children}
             </div>
         )
@@ -37,7 +37,7 @@ export class SubSection extends React.Component {
 export class BtnMain extends React.Component {
     render() {
         return (
-            <button onClick={this.props.onClick} className={"text-sm font-mono bg-blue-600 transition duration-100 ease-in-out hover:bg-blue-500 active:bg-blue-700 rounded-lg px-2 shadow-lg shadow-blue-800/50 hover:shadow-blue-800 active:shadow-inner"+ (this.props.className || "")}>
+            <button onClick={this.props.onClick} className={"text-sm font-mono bg-blue-600 transition duration-100 ease-in-out hover:bg-blue-500 active:bg-blue-700 rounded-lg px-2 shadow-lg shadow-blue-800/50 hover:shadow-blue-800 active:shadow-inner "+ (this.props.className || "")}>
                 {this.props.children}
             </button>
         )
@@ -47,7 +47,7 @@ export class BtnMain extends React.Component {
 export class BtnSecondary extends React.Component {
     render() {
         return (
-            <button onClick={this.props.onClick} className={"text-sm font-mono bg-indigo-600 transition duration-100 ease-in-out hover:bg-indigo-500 active:bg-indigo-700 rounded-lg px-2 shadow-lg shadow-indigo-800/50 hover:shadow-indigo-800 active:shadow-inner"+ (this.props.className || "")}>
+            <button onClick={this.props.onClick} className={"text-sm font-mono bg-indigo-600 transition duration-100 ease-in-out hover:bg-indigo-500 active:bg-indigo-700 rounded-lg px-2 shadow-lg shadow-indigo-800/50 hover:shadow-indigo-800 active:shadow-inner "+ (this.props.className || "")}>
                 {this.props.children}
             </button>
         )
@@ -57,7 +57,7 @@ export class BtnSecondary extends React.Component {
 export class BtnMinim extends React.Component {
     render() {
         return (
-            <button onClick={this.props.onClick} className={"font-mono nav-link block pr-2 lg:px-2 py-2 text-gray-200 hover:text-gray-100 active:text-gray-300 transition duration-100 ease-in-out"+ (this.props.className || "")}>
+            <button onClick={this.props.onClick} className={"font-mono nav-link block pr-2 lg:px-2 py-2 text-gray-200 hover:text-gray-100 active:text-gray-300 transition duration-100 ease-in-out "+ (this.props.className || "")}>
                 {this.props.children}
             </button>
         )
@@ -67,7 +67,7 @@ export class BtnMinim extends React.Component {
 export class H1 extends React.Component {
     render() {
         return (
-            <h1 className={"text-6xl font-extrabold font-mono text-white"+ (this.props.className || "")}>
+            <h1 className={"text-6xl font-extrabold font-mono text-white "+ (this.props.className || "")}>
                 {this.props.children}
             </h1>
         )
@@ -77,7 +77,7 @@ export class H1 extends React.Component {
 export class H2 extends React.Component {
     render() {
         return (
-            <h2 className={"text-4xl font-bold font-mono text-white"+ (this.props.className || "")}>
+            <h2 className={"text-4xl font-bold font-mono text-white "+ (this.props.className || "")}>
                 {this.props.children}
             </h2>
         )
@@ -87,7 +87,7 @@ export class H2 extends React.Component {
 export class H3 extends React.Component {
     render() {
         return (
-            <h3 className={"text-2xl font-semibold font-mono text-white"+ (this.props.className || "")}>
+            <h3 className={"text-2xl font-semibold font-mono text-white "+ (this.props.className || "")}>
                 {this.props.children}
             </h3>
         )
@@ -97,7 +97,7 @@ export class H3 extends React.Component {
 export class P extends React.Component {
     render() {
         return (
-            <p className={"text-sm font-mono text-white"+ (this.props.className || "")}>
+            <p className={"text-sm font-mono text-white "+ (this.props.className || "")}>
                 {this.props.children}
             </p>
         )
@@ -137,7 +137,7 @@ export class TBody extends React.Component {
 export class TR1 extends React.Component {
     render() {
         return (
-            <tr className={"bg-zinc-600 shadow-lg hover:bg-zinc-400 transition duration-100 ease-in-out"+ (this.props.className || "")}>
+            <tr className={"bg-zinc-600 shadow-lg hover:bg-zinc-400 transition duration-100 ease-in-out "+ (this.props.className || "")}>
                 {this.props.children}
             </tr>
         )
@@ -147,7 +147,7 @@ export class TR1 extends React.Component {
 export class TR2 extends React.Component {
     render() {
         return (
-            <tr className={"bg-zinc-500 shadow-lg hover:bg-zinc-400 transition duration-100 ease-in-out"+ (this.props.className || "")}>
+            <tr className={"bg-zinc-500 shadow-lg hover:bg-zinc-400 transition duration-100 ease-in-out "+ (this.props.className || "")}>
                 {this.props.children}
             </tr>
         )
@@ -157,7 +157,7 @@ export class TR2 extends React.Component {
 export class TH extends React.Component {
     render() {
         return (
-            <th className={"text-sm font-mono bg-zinc-700 border-2 border-zinc-800"+ (this.props.className || "")}>
+            <th className={"text-sm font-mono bg-zinc-700 border-2 border-zinc-800 "+ (this.props.className || "")}>
                 {this.props.children}
             </th>
         )
@@ -167,7 +167,7 @@ export class TH extends React.Component {
 export class TD extends React.Component {
     render() {
         return (
-            <td className={"text-sm font-mono border-2 border-zinc-800"+ (this.props.className || "")}>
+            <td className={"text-sm font-mono border-2 border-zinc-800 "+ (this.props.className || "")}>
                 {this.props.children}
             </td>
         )
