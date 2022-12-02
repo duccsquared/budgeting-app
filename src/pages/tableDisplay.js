7d157470d79c0c9284efa6f3d78eb41a8033056e
Mario Susanto <msus0004@student.monash.edu>
2022-12-02 22:08:58 +0700

    created potential design for table data

--- src/pages/tableDisplay.js
+++ src/pages/tableDisplay.js
@@ -1,9 +1,61 @@
-import {MainSection, BtnMain, BtnSecondary, H1, H2, H3, P, Table, TR1, TR2, TH, TD, CheckBox, RadioBox} from '../components.js';
+import {MainSection, BtnMain, BtnSecondary, H1, H2, H3, P, Table, THead, TBody, TR1, TR2, TH, TD, CheckBox, RadioBox, SubSection} from '../components.js';
 
 function TableDisplay() {
     return (
         <MainSection>
-            <H3>Table</H3>
+            <H2>Table Data</H2>
+            <div className="grid grid-cols-6 gap-4">
+                <SubSection className="col-span-6"></SubSection>
+                <SubSection className="col-span-4 row-span-2">
+                    <Table className="table-fixed">
+                        <THead>
+                            <TR1>
+                                <TH className="w-1/12">Date</TH>
+                                <TH className="w-2/12">Label</TH>
+                                <TH className="w-1/12">Amount</TH>
+                                <TH className="w-2/12">Category</TH>
+                                <TH className="w-5/12">Description</TH>
+                                <TH className="w-1/12"></TH>
+                            </TR1>
+                        </THead>
+                        <TBody>
+                            <TR1>
+                                <TD>22/11/2022</TD>
+                                <TD>Lunch</TD>
+                                <TD>5</TD>
+                                <TD>Food</TD>
+                                <TD>Chicken rice at uni</TD>
+                                <TD><BtnMain>X</BtnMain></TD>
+                            </TR1>
+                        </TBody>
+
+                    </Table> 
+                </SubSection>
+                <SubSection>
+                    <P>data type</P>
+                    <RadioBox name="dataType" value="net">net cashflow</RadioBox>
+                    <RadioBox name="dataType" value="in">inflow</RadioBox>
+                    <RadioBox name="dataType" value="out">outflow</RadioBox>
+                </SubSection>
+                <SubSection>    
+                    <P>accounts</P>
+                    <CheckBox value="">cash</CheckBox>
+                    <CheckBox value="">bank</CheckBox>
+                    <CheckBox value="">steam</CheckBox>
+                </SubSection>
+                <SubSection>
+                    <P>clustering</P>
+                    <RadioBox name="clustering" value="sep">separate entities</RadioBox>
+                    <RadioBox name="clustering" value="day">cluster by day</RadioBox>
+                    <RadioBox name="clustering" value="month">cluster by month</RadioBox>
+                </SubSection>
+                <SubSection>
+                    <P>categories</P>
+                    <CheckBox value="">food</CheckBox>
+                    <CheckBox value="">groceries</CheckBox>
+                    <CheckBox value="">furniture</CheckBox>
+                </SubSection>
+            </div>
         </MainSection>
     )
 }
