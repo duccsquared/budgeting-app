51655dac8168a710e413bfaf3753d1191c33f9bf
Mario Susanto <msus0004@student.monash.edu>
2022-12-28 14:37:49 +0700

    moved data sections from tableDisplay to a separate file

---
+++ src/dataOptionComponents.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {MainSection, BtnMain, BtnSecondary, H1, H2, H3, P, Table, THead, TBody, TR1, TR2, TH, TD, CheckBox, RadioBox, SubSection, BtnMinim, InputDate} from './components.js';
+
+export default class DataTypeSection extends React.Component {
+    render() {
+        return (
+            <SubSection>
+                <P>data type</P>
+                <RadioBox name="dataType" value="net" onChange={()=>this.props.onRadio("net")}>net cashflow</RadioBox>
+                <RadioBox name="dataType" value="in" onChange={()=>this.props.onRadio("in")}>inflow</RadioBox>
+                <RadioBox name="dataType" value="out" onChange={()=>this.props.onRadio("out")}>outflow</RadioBox>
+            </SubSection>
+        )
+    }
+}
+
+export class DateRangeSection extends React.Component {
+    render() {
+        return (
+            <SubSection>
+                <P>date range</P>
+                <div className="inline-flex">
+                    <P>start:</P>
+                    <div className="w-2"/>
+                    <InputDate onChange={(e) => this.props.setStartDate(e)}/>
+                </div>
+                <div className="inline-flex">
+                    <P>end:</P>
+                    <div className="w-2"/>
+                    <InputDate onChange={(e) => this.props.setEndDate(e)}/>
+                </div>
+            </SubSection>
+        )
+    }
+}
+
+export class CategorySection extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isInitial: true
+        }
+    }
+    render() {
+        let categoryList = this.props.categoryList
+        let html = (
+            <SubSection>
+                <P>categories</P>
+                {categoryList.map((category) => <CheckBox value="" onChange={(e)=>{category.checked=!category.checked;this.props.update()}} {...this.state.isInitial?{checked: category.checked}:{}}>{category.name}</CheckBox>)}
+            </SubSection>
+        )
+        if(this.state.isInitial) {
+            this.setState({isInitial: false})
+        }
+        return html
+    }
+}
+
+export class AccountSection extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isInitial: true
+        }
+    }
+    render() {
+        let accountList = this.props.accountList
+        let html =  (
+            <SubSection>
+                <P>accounts</P>
+                {accountList.map((account) => <CheckBox value=""  onChange={(e)=>{account.checked=!account.checked;this.props.update()}} {...this.state.isInitial?{checked: account.checked}:{}}>{account.name}</CheckBox>)}
+            </SubSection>
+        )
+        if(this.state.isInitial) {
+            this.setState({isInitial: false})
+        }
+        return html
+    }
+}
--- src/pages/tableDisplay.js
+++ src/pages/tableDisplay.js
@@ -4,7 +4,7 @@ Page that displays the transaction data in the form of a table
 import React from 'react';
 import {MainSection, BtnMain, BtnSecondary, H1, H2, H3, P, Table, THead, TBody, TR1, TR2, TH, TD, CheckBox, RadioBox, SubSection, BtnMinim, InputDate} from '../components.js';
 import TransactionTable from '../transactionTable.js';
-
+import DataTypeSection, { AccountSection, CategorySection, DateRangeSection } from '../dataOptionComponents.js';
 class TableDisplay extends React.Component {
     constructor(props) {
         super(props)
@@ -57,81 +57,6 @@ class TableDisplay extends React.Component {
     }
 }
 
-class DataTypeSection extends React.Component {
-    render() {
-        return (
-            <SubSection>
-                <P>data type</P>
-                <RadioBox name="dataType" value="net" onChange={()=>this.props.onRadio("net")}>net cashflow</RadioBox>
-                <RadioBox name="dataType" value="in" onChange={()=>this.props.onRadio("in")}>inflow</RadioBox>
-                <RadioBox name="dataType" value="out" onChange={()=>this.props.onRadio("out")}>outflow</RadioBox>
-            </SubSection>
-        )
-    }
-}
 
-class DateRangeSection extends React.Component {
-    render() {
-        return (
-            <SubSection>
-                <P>date range</P>
-                <div className="inline-flex">
-                    <P>start:</P>
-                    <div className="w-2"/>
-                    <InputDate onChange={(e) => this.props.setStartDate(e)}/>
-                </div>
-                <div className="inline-flex">
-                    <P>end:</P>
-                    <div className="w-2"/>
-                    <InputDate onChange={(e) => this.props.setEndDate(e)}/>
-                </div>
-            </SubSection>
-        )
-    }
-}
-
-class CategorySection extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isInitial: true
-        }
-    }
-    render() {
-        let categoryList = this.props.categoryList
-        let html = (
-            <SubSection>
-                <P>categories</P>
-                {categoryList.map((category) => <CheckBox value="" onChange={(e)=>{category.checked=!category.checked;this.props.update()}} {...this.state.isInitial?{checked: category.checked}:{}}>{category.name}</CheckBox>)}
-            </SubSection>
-        )
-        if(this.state.isInitial) {
-            this.setState({isInitial: false})
-        }
-        return html
-    }
-}
-
-class AccountSection extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isInitial: true
-        }
-    }
-    render() {
-        let accountList = this.props.accountList
-        let html =  (
-            <SubSection>
-                <P>accounts</P>
-                {accountList.map((account) => <CheckBox value=""  onChange={(e)=>{account.checked=!account.checked;this.props.update()}} {...this.state.isInitial?{checked: account.checked}:{}}>{account.name}</CheckBox>)}
-            </SubSection>
-        )
-        if(this.state.isInitial) {
-            this.setState({isInitial: false})
-        }
-        return html
-    }
-}
 
 export default TableDisplay
